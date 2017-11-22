c4bd64c4042ad4b896d4c564b61f3b95482e10f7
Michael Chen <mechiland@gmail.com>
2017-11-22 12:12:02 +0800

    add draft layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,24 @@
-import React from "react"
-import Link from "gatsby-link"
-import {Container, Row, Col} from "reactstrap"
+import React from "react";
+import Link from "gatsby-link";
+import { Container, Row, Col } from "reactstrap";
+
+const HomeBlock = ( props ) => (
+  <div backgroundColor={props.bgColor}>
+    {props.name}
+  </div>
+);
 
 export default ({ data }) => {
-
   return (
     <Container>
       <Row>
-        <Col md={4}>品牌</Col>
-        <Col md={4}>市场</Col>
-        <Col md={4}>产品</Col>
+        <Col md={4} xs={12}><HomeBlock bgColor="red" name="品牌" /></Col>
+        <Col md={4} xs={12}><HomeBlock bgColor="red" name="市场" /></Col>
+        <Col md={4} xs={12}><HomeBlock bgColor="red" name="产品" /></Col>
       </Row>
-
-      <Row>
-        <Col xs={12}>
-          <h4>
-        {data.allMarkdownRemark.totalCount} Posts
-      </h4>
-      {data.allMarkdownRemark.edges.map(({ node }) =>
-        <div key={node.id}>
-        <Link to={node.fields.slug}>
-          <h3>
-            {node.frontmatter.title}{" "}
-            <span color="#BBB">— {node.frontmatter.date}</span>
-          </h3>
-          </Link>
-          <p>
-            {node.excerpt}
-          </p>
-        </div>
-      )}
-        </Col>
-      </Row>
-
     </Container>
-
-  )
-}
+  );
+};
 
 export const query = graphql`
   query IndexQuery {
@@ -57,4 +39,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
